36fe9aa920a56bc072412fdbe940679fb3a1349f
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-01-22 14:20:29 +0000

    removed a file that is not compiling

--- plugins/TauGenJetEventSelector.cc
+++
@@ -1,9 +0,0 @@
-#include "TauAnalysis/GenSimTools/plugins/TauGenJetEventSelector.h"
-
-#include "PhysicsTools/UtilAlgos/interface/EventSelectorBase.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, TauGenJetMinEventSelector, "TauGenJetMinEventSelector");
-
-DEFINE_EDM_PLUGIN(EventSelectorPluginFactory, TauGenJetMaxEventSelector, "TauGenJetMaxEventSelector");

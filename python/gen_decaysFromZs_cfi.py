e424064db78898d82e7e1085a8c9d434016caaab
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-03-20 16:53:27 +0000

    added sequence for selection of gen. Z --> ee, Z --> mumu and Z --> tautau decays

--- python/gen_decaysFromZs_cfi.py
+++ python/gen_decaysFromZs_cfi.py
@@ -1,5 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
+#################################################################################
+#
+# CV: obsolete; kept only for backwards compatibility reasons
+#
 decaysFromZs = cms.EDProducer(
     "GenParticlePruner",
     src = cms.InputTag("genParticles"),
@@ -9,3 +13,44 @@ decaysFromZs = cms.EDProducer(
     "drop pdgId = {Z0}"
     )
 )
+#################################################################################
+
+genParticlesFromZs = cms.EDProducer("GenParticlePruner",
+  src = cms.InputTag("genParticles"),
+  select = cms.vstring(
+    "drop * ", # this is the default
+    "keep+ pdgId = {Z0}",
+    "drop pdgId = {Z0}"
+  )
+)
+
+genElectronsFromZs = cms.EDProducer("GenParticlePruner",
+  src = cms.InputTag("genParticlesFromZs"),
+  select = cms.vstring(
+    "drop * ", # this is the default
+    "keep pdgId = {e+}",
+    "keep pdgId = {e-}"
+  )
+)
+
+genMuonsFromZs = cms.EDProducer("GenParticlePruner",
+  src = cms.InputTag("genParticlesFromZs"),
+  select = cms.vstring(
+    "drop * ", # this is the default
+    "keep pdgId = {mu+}",
+    "keep pdgId = {mu-}"
+  )
+)
+
+genTausFromZs = cms.EDProducer("GenParticlePruner",
+  src = cms.InputTag("genParticlesFromZs"),
+  select = cms.vstring(
+    "drop * ", # this is the default
+    "keep pdgId = {tau-}",
+    "keep pdgId = {tau+}"
+  )
+)
+
+produceGenDecayProductsFromZs = cms.Sequence(genParticlesFromZs * genElectronsFromZs * genMuonsFromZs * genTausFromZs)
+
+

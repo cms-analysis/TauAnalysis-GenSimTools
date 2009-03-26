d7ffcaed8bf7dd183a67010963a22e42a2d5a840
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-03-26 13:44:18 +0000

    removed "ForTauAnalyses" from name of TauGenJet collection

--- python/tauGenJetProducer_cff.py
+++ python/tauGenJetProducer_cff.py
@@ -7,13 +7,13 @@ from PhysicsTools.JetMCAlgos.TauGenJets_cfi import *
 # import config for selection of generated tau-jets
 from TauAnalysis.GenSimTools.tauGenJetSelector_cfi import *
 
-produceTauGenJetsForTauAnalyses = cms.Sequence( tauGenJets
-                                               *selectedGenTauDecaysToElectron
-                                               *selectedGenTauDecaysToElectronEta21Cumulative * selectedGenTauDecaysToElectronEta21Individual
-                                               *selectedGenTauDecaysToElectronPt15Cumulative * selectedGenTauDecaysToElectronPt15Individual
-                                               *selectedGenTauDecaysToMuon
-                                               *selectedGenTauDecaysToMuonEta21Cumulative * selectedGenTauDecaysToMuonEta21Individual
-                                               *selectedGenTauDecaysToMuonPt15Cumulative * selectedGenTauDecaysToMuonPt15Individual
-                                               *selectedGenTauDecaysToHadrons
-                                               *selectedGenTauDecaysToHadronsEta21Cumulative * selectedGenTauDecaysToHadronsEta21Individual
-                                               *selectedGenTauDecaysToHadronsPt20Cumulative * selectedGenTauDecaysToHadronsPt20Individual )
+produceTauGenJets = cms.Sequence( tauGenJets
+                                 *selectedGenTauDecaysToElectron
+                                 *selectedGenTauDecaysToElectronEta21Cumulative * selectedGenTauDecaysToElectronEta21Individual
+                                 *selectedGenTauDecaysToElectronPt15Cumulative * selectedGenTauDecaysToElectronPt15Individual
+                                 *selectedGenTauDecaysToMuon
+                                 *selectedGenTauDecaysToMuonEta21Cumulative * selectedGenTauDecaysToMuonEta21Individual
+                                 *selectedGenTauDecaysToMuonPt15Cumulative * selectedGenTauDecaysToMuonPt15Individual
+                                 *selectedGenTauDecaysToHadrons
+                                 *selectedGenTauDecaysToHadronsEta21Cumulative * selectedGenTauDecaysToHadronsEta21Individual
+                                 *selectedGenTauDecaysToHadronsPt20Cumulative * selectedGenTauDecaysToHadronsPt20Individual )

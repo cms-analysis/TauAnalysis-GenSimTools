6e029d4e1afac0a43053d95a7b0260be18eb6564
Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch>
2009-08-04 16:40:18 +0000

    small modification of test cfg

--- test/analysis_Z_cfg.py
+++ test/analysis_Z_cfg.py
@@ -2,12 +2,23 @@
 
 import FWCore.ParameterSet.Config as cms
 
+# Choose analysis mode:
+# 0: Z->mu,mu 
+# 1: Z->tau,tau->mu,tau-jet
+mode = 0
 
 process = cms.Process("ANA")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
 
-process.load("Configuration.Generator.ZMM_cfi")
+if mode==0:
+    print "*** Z->mu,mu gen-analysis ***"
+    process.load("Configuration.Generator.ZMM_cfi")
+else:
+    print "*** Z->tau,tau->mu,tau-jet gen-analysis ***"
+    process.load("Configuration.Generator.ZTT_Tauola_OneLepton_OtherHadrons_cfi")
+    process.generator.ExternalDecays.Tauola.InputCards.mdtau = 116
+
 process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
 
 # analysis
@@ -19,16 +30,25 @@ process.load("TauAnalysis.GenSimTools.gen_cff")
 process.particleListDrawer.maxEventsToPrint = cms.untracked.int32(10)
 process.particleListDrawer.printOnlyHardInteraction = True
 
-process.load("TauAnalysis.GenSimTools.genZllReconstruction_cff")
-
-process.genZll.verbosity = 1
-
+if mode==0:
+    process.load("TauAnalysis.GenSimTools.genZllReconstruction_cff")
+    process.genZll.verbosity = 1
+else:
+    process.load("TauAnalysis.GenSimTools.genDiTauReconstruction_cff")
+    process.genDiTau.verbosity = 0
+    
 process.p1 = cms.Path(
     process.ProductionFilterSequence + 
     process.genParticles +
-    process.genParticlesPrint +
-    process.genZllReconstruction
+    process.genParticlesPrint
     )
+if mode==0:
+    process.p1 += process.genZllReconstruction
+else:
+    process.load("RecoMET.Configuration.GenMETParticles_cff")
+    process.load("RecoMET.METProducers.genMetTrue_cfi")
+    process.p1 += process.genParticlesForMETAllVisible*process.genMetTrue
+    process.p1 += process.genDiTauReconstruction
 
 
 process.out = cms.OutputModule("PoolOutputModule",
@@ -48,6 +68,4 @@ process.outpath = cms.EndPath( process.out )
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
-
-
 

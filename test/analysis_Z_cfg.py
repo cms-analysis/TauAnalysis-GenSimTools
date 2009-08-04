c4f9dad5bdcc0f4bdcb2a546f2ae1f48adea4c0a
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-08-04 13:20:24 +0000

    ported to 3_1_X

--- test/analysis_Z_cfg.py
+++ test/analysis_Z_cfg.py
@@ -7,13 +7,8 @@ process = cms.Process("ANA")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
 
-
-#process.source = cms.Source("PoolSource",
-#                            fileNames = cms.untracked.vstring('file:PATLayer1_ZToTaus.root')
-#                            fileNames = cms.untracked.vstring('file:PATLayer1_fromAOD_ZtoMus.root')
-#                            )
-
-process.load("TauAnalysis.GenSimTools.Sources.gensource_ZtoMus_PYTHIA_cfi")
+process.load("Configuration.Generator.ZMM_cfi")
+process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
 
 # analysis
 process.load("TauAnalysis.GenSimTools.gen_cff")
@@ -24,16 +19,14 @@ process.load("TauAnalysis.GenSimTools.gen_cff")
 process.particleListDrawer.maxEventsToPrint = cms.untracked.int32(10)
 process.particleListDrawer.printOnlyHardInteraction = True
 
-process.load("TauAnalysis.GenSimTools.genDiTauReconstruction_cff")
 process.load("TauAnalysis.GenSimTools.genZllReconstruction_cff")
-
 
 process.genZll.verbosity = 1
 
 process.p1 = cms.Path(
-    process.genParticles + 
+    process.ProductionFilterSequence + 
+    process.genParticles +
     process.genParticlesPrint +
-#    process.genDiTauReconstruction + 
     process.genZllReconstruction
     )
 
@@ -54,12 +47,6 @@ process.outpath = cms.EndPath( process.out )
 
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.threshold = 'INFO'
-process.MessageLogger.categories.append('PATLayer0Summary')
-process.MessageLogger.cerr.INFO = cms.untracked.PSet(
-    default          = cms.untracked.PSet( limit = cms.untracked.int32(0) ),
-    PATLayer0Summary = cms.untracked.PSet( limit = cms.untracked.int32(10) )
-)
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
 

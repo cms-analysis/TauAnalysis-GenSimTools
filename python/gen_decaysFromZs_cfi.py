079b3e7d94f3b746fdf9781af4b839cc43261c18
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-01-19 13:58:08 +0000

    fixed logic (and updates names of flags) of Ztautau gen. decay mode information (for acceptance, purity and stability computation)

--- python/gen_decaysFromZs_cfi.py
+++ python/gen_decaysFromZs_cfi.py
@@ -1,12 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
+#--------------------------------------------------------------------------------
+# select collections of generated electrons, muons and tau leptons
+# resulting from Z-boson decays
+#--------------------------------------------------------------------------------
+
 genParticlesFromZs = cms.EDProducer("GenParticlePruner",
-  src = cms.InputTag("genParticles"),
-  select = cms.vstring(
-    "drop * ", # this is the default
-    "keep+ pdgId = {Z0}",
-    "drop pdgId = {Z0}"
-  )
+    src = cms.InputTag("genParticles"),
+    select = cms.vstring(
+        "drop * ", # this is the default
+        "keep+ pdgId = {Z0}",
+        "drop pdgId = {Z0}"
+    )
 )
 
 genElectronsFromZs = cms.EDProducer("GenParticlePruner",
@@ -36,57 +41,89 @@ genTausFromZs = cms.EDProducer("GenParticlePruner",
   )
 )
 
-genParticlesFromTauonicZdecays = cms.EDProducer("GenParticlePruner",
-  src = cms.InputTag("genTausFromZs"),
-  select = cms.vstring(
-    "drop * ",
-    "keep+ pdgId = {tau+}",
-    "keep+ pdgId = {tau-}",
-    "drop pdgId = {tau+}",
-    "drop pdgId = {tau-}"
-  )
+#--------------------------------------------------------------------------------
+# match tau leptons resulting from Z-boson decays to generator level tau-jets
+#--------------------------------------------------------------------------------
+
+genTauJetsFromZs = cms.EDProducer("TauGenJetMatchSelector",
+  srcGenTauLeptons = cms.InputTag("genTausFromZs"),
+  srcGenParticles = cms.InputTag("genParticles"),
+  srcTauGenJets = cms.InputTag("tauGenJets"),
+  dRmatchGenParticle = cms.double(0.1),
+  dRmatchTauGenJet = cms.double(0.3)
 )
 
-genElectronsFromTauonicZdecays = cms.EDProducer("GenParticlePruner",
-  src = cms.InputTag("genParticlesFromTauonicZdecays"),
-  select = cms.vstring(
-    "drop * ",
-    "keep pdgId = {e+}",
-    "keep pdgId = {e-}",
-  )
+#--------------------------------------------------------------------------------
+# separate tau lepton decays into categories
+#  o tau --> electron nu nu
+#  o tau --> muon nu nu
+#  o tau --> hadrons (mostly charged and neutral pions) nu
+# 
+# NOTE: list of individual tau decay modes to be used
+#       for 'select' configuation parameter of TauGenJetDecayModeSelector module
+#       define in PhysicsTools/JetMCUtils/src/JetMCTag.cc
+#--------------------------------------------------------------------------------
+
+genElectronsFromZtautauDecays = cms.EDFilter("TauGenJetDecayModeSelector",
+    src = cms.InputTag("genTauJetsFromZs"),
+    select = cms.vstring(
+        "electron"
+    ),
+    filter = cms.bool(False)                                       
 )
 
-genMuonsFromTauonicZdecays = cms.EDProducer("GenParticlePruner",
-  src = cms.InputTag("genParticlesFromTauonicZdecays"),
-  select = cms.vstring(
-    "drop * ",
-    "keep pdgId = {mu+}",
-    "keep pdgId = {mu-}"
-  )
+genMuonsFromZtautauDecays = cms.EDFilter("TauGenJetDecayModeSelector",
+    src = cms.InputTag("genTauJetsFromZs"),
+    # list of individual tau decay modes to be used for 'select' configuation parameter
+    # define in PhysicsTools/JetMCUtils/src/JetMCTag.cc
+    select = cms.vstring(
+        "muon"
+    ),
+    filter = cms.bool(False)                                       
 )
 
-genHadronsFromTauonicZdecays = cms.EDProducer("GenParticlePruner",
-  src = cms.InputTag("genParticlesFromTauonicZdecays"),
-  select = cms.vstring(
-    "keep * ",
-    "drop pdgId = {nu_tau}",
-    "drop pdgId = {nu_taubar}",
-    "drop pdgId = {mu+}",
-    "drop pdgId = {mu-}",
-    "drop pdgId = {nu_mu}",
-    "drop pdgId = {nu_mubar}",
-    "drop pdgId = {e+}",
-    "drop pdgId = {e-}",
-    "drop pdgId = {nu_e}",
-    "drop pdgId = {nu_ebar}"
-  )
+genHadronsFromZtautauDecays = cms.EDFilter("TauGenJetDecayModeSelector",
+    src = cms.InputTag("genTauJetsFromZs"),
+    select = cms.vstring(
+        "oneProng0Pi0",
+        "oneProng1Pi0",
+        "oneProng2Pi0",
+        "oneProngOther",
+        "threeProng0Pi0",
+        "threeProng1Pi0",
+        "threeProngOther",
+        "rare"
+    ),
+    filter = cms.bool(False)                                       
+)
+
+#--------------------------------------------------------------------------------
+# select tau lepton decay products that are within detector acceptance
+# (in terms of Pt and eta)
+#--------------------------------------------------------------------------------
+
+genElectronsFromZtautauDecaysWithinAcceptance = cms.EDFilter("GenJetSelector",
+  src = cms.InputTag("genElectronsFromZtautauDecays"),
+  cut = cms.string('pt > 15. && abs(eta) < 2.1')
+)
+
+genMuonsFromZtautauDecaysWithinAcceptance = cms.EDFilter("GenJetSelector",
+  src = cms.InputTag("genMuonsFromZtautauDecays"),
+  cut = cms.string('pt > 15. && abs(eta) < 2.1')
+)
+
+genHadronsFromZtautauDecaysWithinAcceptance = cms.EDFilter("GenJetSelector",
+  src = cms.InputTag("genHadronsFromZtautauDecays"),
+  cut = cms.string('pt > 20. && abs(eta) < 2.1')
 )
 
 produceGenDecayProductsFromZs = cms.Sequence(
     genParticlesFromZs
    * genElectronsFromZs * genMuonsFromZs * genTausFromZs
-   * genParticlesFromTauonicZdecays
-   * genElectronsFromTauonicZdecays * genMuonsFromTauonicZdecays * genHadronsFromTauonicZdecays
+   * genTauJetsFromZs
+   * genElectronsFromZtautauDecays * genElectronsFromZtautauDecaysWithinAcceptance
+   * genMuonsFromZtautauDecays * genMuonsFromZtautauDecaysWithinAcceptance
+   * genHadronsFromZtautauDecays * genHadronsFromZtautauDecaysWithinAcceptance
 )
 
 
